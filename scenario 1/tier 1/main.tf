4d050899bb357a21c738f2cb988ea21eb82f5f5d
Jan Schönfeld <jan.schoenfeld@tutanota.de>
2023-06-08 13:42:44 +0200

    fixed cidr ip conflict

--- scenario 1/tier 1/main.tf
+++ scenario 1/tier 1/main.tf
@@ -32,7 +32,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.1.0/28"
   availability_zone = eu-central-1a
   availability_zone = eu-central-1b
 
@@ -41,9 +41,20 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "appserver" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.2.0/28"
+  availability_zone = eu-central-1a
+  availability_zone = eu-central-1b
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_subnet" "db" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/28"
   availability_zone = eu-central-1a
   availability_zone = eu-central-1b
 
@@ -54,18 +65,7 @@ resource "aws_subnet" "private" {
 
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.3.0/24"
-  availability_zone = eu-central-1a
-  availability_zone = eu-central-1b
-
-  tags = {
-    Name = "private-subnet"
-  }
-}
-
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "14.0.4.0/24"
+  cidr_block = "10.2.0.0/28"
   availability_zone = eu-central-1a
   availability_zone = eu-central-1b
 
