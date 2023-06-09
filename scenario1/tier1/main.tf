d3af19f263f995eb9e65cfde43d4e5f688d4ff57
Jan Schönfeld <jan.schoenfeld@tutanota.de>
2023-06-09 12:21:01 +0200

    moved the comments to plan.txt and /hashicorp-tutorial

--- scenario1/tier1/main.tf
+++ scenario1/tier1/main.tf
@@ -1,9 +1,3 @@
-// This is a single-line comment
-/*
-multi-line 
-comment
-*/
-
 terraform {
   required_providers {
     aws = {
@@ -13,16 +7,9 @@ terraform {
   }
 }
 
-/*
-Even though it's not required, it's highly recommended to include the terraform block in your main.tf file for better consistency, reliability, and compatibility.
-*/
-
 provider "aws" {
   region = "eu-central-1"
 }
-
-// Your resources and configurations go here
-
 
 resource "aws_vpc" "main" {
   cidr_block = "172.31.0.0/16"
@@ -69,27 +56,5 @@ resource "aws_subnet" "datatier-b" {
 }
 
 
-/*
-Make VPC
-~~● 4 subnets (1 public, 3 private)~~
-~~● Enable in subnet settings public ip addresses~~
-~~● Make it highly available (use 2 availability zones, the final private subnet can be the only
-one in a different subnet)~~
 
-● Allocate an Elastic IP
-● Create a nat gateway
-● Create an internet gateway and attach it to your VPC
-
-● Make route tables for your public and private subnets and attach an internet gateway
-and nat gateway to them respectively
-● Make security groups for Bastion Host, web server, app server, and database
-● Make sure to go back to security groups after making them and adding security groups
-to link them together, for example in the app server security group adding a rule for the
-database security group after creating the database security group.
-*/
-
-
-/*
-current work: steps 4,5 & 6
-*/
 
