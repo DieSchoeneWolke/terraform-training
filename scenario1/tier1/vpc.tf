0bd0cacd06d8cd3d5787387d2e4dd094c8fa2a31
DieSchoeneWolke <jan.schoenfeld@tutanota.de>
2023-06-13 12:42:56 +0200

    added route tables

--- scenario1/tier1/vpc.tf
+++ scenario1/tier1/vpc.tf
@@ -65,23 +65,65 @@ resource "aws_eip" "vpc" {
 // Update: Enabled it again beacuse using an elastic IP incurs some costs when it's not attached! 
 // $0.005 per Elastic IP address not attached to a running instance per hour
 
-resource "aws_nat_gateway" "main" {
+resource "aws_nat_gateway" "main_ngw" {
   allocation_id = "eipalloc-094f3c5ef0a632d99"
   subnet_id = aws_subnet.presentationtier-a.id
   tags = {
-    Name = "gw NAT"
+    Name = "main"
   }
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.gw]
+  depends_on = [aws_internet_gateway.main_igw]
 
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "main_igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "main"
   }
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main_igw.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route" "private_route" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main_ngw.id
+}
+
+resource "aws_route_table_association" "presentationtier_a_association" {
+  subnet_id      = aws_subnet.presentationtier-a.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "apptier_a_association" {
+  subnet_id      = aws_subnet.apptier-a.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "datatier_a_association" {
+  subnet_id      = aws_subnet.datatier-a.id
+  route_table_id = aws_route_table.private_rt.id
 }
